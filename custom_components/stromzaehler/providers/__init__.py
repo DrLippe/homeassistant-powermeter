6f2a8cba8285ff5b9832361fd308cf41cac46c17
DrLippe <echterbeck.philipp@gmail.com>
2026-08-17 09:40:55 +0200

    Add provider package

---
+++ custom_components/stromzaehler/providers/__init__.py
@@ -0,0 +1,5 @@
+"""Provider adapters for future meter-reading submissions."""
+
+from .base import MeterReadingProvider, MeterReadingSubmission
+
+__all__ = ["MeterReadingProvider", "MeterReadingSubmission"]
