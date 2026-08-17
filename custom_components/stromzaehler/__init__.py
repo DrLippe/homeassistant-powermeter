9a3c2d32775a194ebd1d8cf1473620a0c0369c18
DrLippe <echterbeck.philipp@gmail.com>
2026-08-17 09:40:20 +0200

    Use options flow automatic reload

--- custom_components/stromzaehler/__init__.py
+++ custom_components/stromzaehler/__init__.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
-from .const import DOMAIN, PLATFORMS
+from .const import PLATFORMS
 from .manager import StromzaehlerManager
 
 
@@ -19,7 +19,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: StromzaehlerConfigEntry)
     entry.runtime_data = manager
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
-    entry.async_on_unload(entry.add_update_listener(_async_update_listener))
     return True
 
 
@@ -29,8 +28,3 @@ async def async_unload_entry(hass: HomeAssistant, entry: StromzaehlerConfigEntry
     if unload_ok:
         await entry.runtime_data.async_stop()
     return unload_ok
-
-
-async def _async_update_listener(hass: HomeAssistant, entry: StromzaehlerConfigEntry) -> None:
-    """Reload the integration after options are changed."""
-    await hass.config_entries.async_reload(entry.entry_id)
